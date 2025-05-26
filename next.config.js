02f904eea57cfb8d8af3fe6429365e53097820f9
Lutz Dietterich <lutzdietterich@googlemail.com>
2025-05-26 14:47:20 +0200

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-  images: {
-    remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'images.unsplash.com'
-      // },
-    ],
-  },
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+    },
+    images: {
+        remotePatterns: [
+            // {
+            //   protocol: 'https',
+            //   hostname: 'images.unsplash.com'
+            // },
+        ],
+    },
 };
 
 module.exports = nextConfig;
