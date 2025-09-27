1dc057a978e460310b98255abd3021be4e31b34d
Lutz Dietterich <lutzdietterich@googlemail.com>
2025-09-27 12:58:11 +0200

    add next config for static export

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-  images: {
-    remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'images.unsplash.com'
-      // },
-    ],
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        styledComponents: true,
+    },
+    images: {
+        remotePatterns: [
+            // {
+            //   protocol: 'https',
+            //   hostname: 'images.unsplash.com'
+            // },
+        ],
+        unoptimized: true, // Wichtig für statischen Export
+    },
+    output: "export", // Aktiviert statischen Export
+    trailingSlash: true, // URLs enden mit /
 };
 
 module.exports = nextConfig;
